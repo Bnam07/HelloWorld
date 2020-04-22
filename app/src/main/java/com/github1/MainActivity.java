385f18fbc2c7b2c9c84327954ed82f623c33c623
Bnam <bnam12345@gmail.com>
2020-04-22 12:41:16 +0430

    add to masterrrr

--- app/src/main/java/com/github1/MainActivity.java
+++ app/src/main/java/com/github1/MainActivity.java
@@ -11,6 +11,7 @@ public class MainActivity extends AppCompatActivity {
     this is my second commit
     this is my third commit - direct push
     this is my forth commit from dev   22
+    this is another for master
      */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
