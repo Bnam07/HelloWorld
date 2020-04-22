d269ff4ccafa63a526a68847a50df62642c12c75
Bnam <bnam12345@gmail.com>
2020-04-22 12:53:22 +0430

    add 543210 to master

--- app/src/main/java/com/github1/MainActivity.java
+++ app/src/main/java/com/github1/MainActivity.java
@@ -12,7 +12,7 @@ public class MainActivity extends AppCompatActivity {
     this is my third commit - direct push
     this is my forth commit from dev   22
     this is another for master
-    54321
+    543210
      */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
