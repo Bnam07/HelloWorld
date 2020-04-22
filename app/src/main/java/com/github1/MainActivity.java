43b306909e60ef10f6155436043f7e95c60e7f9c
Bnam <bnam12345@gmail.com>
2020-04-22 11:52:39 +0430

    first commit

---
+++ app/src/main/java/com/github1/MainActivity.java
@@ -0,0 +1,17 @@
+package com.github1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    /*
+    this is first commit
+     */
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
